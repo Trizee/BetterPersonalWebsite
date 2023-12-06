4d22faed96decb16fb2a9a75fb94205b7c52ce5f
Tri <trihoang09@gmail.com>
2023-12-06 14:22:18 -0700

    setting up dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // Theme Setter 
+  const [theme,setTheme] = useState(null);
+
+  // Theme Checker
+  useEffect(()=>{
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      setTheme("dark")
+    } else {
+      setTheme("light")
+    }
+  },[])
+
+  // Theme Toggle
+  useEffect(()=>{
+    if(theme === "dark"){
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    } 
+  },[theme])
+
+  const handleClick = () => {
+    setTheme(theme === "dark"? "light" : "dark")
+  }
+
+  console.log(theme)
 
   return (
-    <>
-      <h1 className='text-center float-right'>Hello</h1>
-    </>
+    <div className='h-screen bg-gray-200 dark:bg-gray-950 transition-all duration-500'>
+      <h1 className='text-center'>Hello</h1>
+      <button className="border-l-neutral-800 bg-red-700 rounded-full p-2 flex justify-center m-auto" onClick={handleClick}> darkmode</button>
+    </div>
   )
 }
 
