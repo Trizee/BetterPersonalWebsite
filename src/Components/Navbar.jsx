8839209baec4ced6493260f4e2f7724b71815705
Tri Hoang <trihoang09@gmail.com>
2023-12-13 12:50:56 -0700

    making navbar mobile responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import { MdLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
 import { useState,useEffect } from "react";
 import { GoHomeFill } from "react-icons/go";
-import { FaCode } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
 import { IoInformationCircle } from "react-icons/io5";
 
 export default function NavBar({theme,setTheme}){
@@ -11,9 +11,9 @@ export default function NavBar({theme,setTheme}){
     
     useEffect(()=>{
         if (theme === "dark"){
-            setStyle({ color: "#e5e7eb", })
+            setStyle({ color: "#e5e7eb", transition: 'transform 0.3s ease-in-out'})
         } else {
-            setStyle({ color: "#1f2937", })
+            setStyle({ color: "#1f2937",transition: 'transform 0.3s ease-in-out' })
         }
     },[theme])
 
@@ -21,12 +21,12 @@ export default function NavBar({theme,setTheme}){
 
     return (
         <>
-        <div className="h-screen w-16 fixed z-10 top-0 left-0 flex flex-col content-center items-center justify-center">
-                <div className="p-1 shadow-md rounded-md bg-sky-900 transition-all ease-linear duration-500 h-1/4 justify-evenly flex flex-col">
-                <GoHomeFill size="3rem"/>
-                <FaCode size="3rem"/>
-                <IoInformationCircle size="3rem"/>
-                {theme === "dark" ? <MdLightMode style={style} size="3rem" onClick={()=>{setTheme("light")}} /> : <MdDarkMode style={style} size="3rem" onClick={()=>{setTheme("dark")}}/>}
+        <div className=" md:w-16 relative md:h-screen md:fixed z-10 top-0 left-0 flex flex-col content-center md:items-center md:justify-center">
+                <div className="p-3 border-gray-900 dark:border-gray-400 md:border-2 md:shadow-lg md:rounded-r-lg transition-all duration-500 md:h-1/2 justify-center gap-8 md:justify-evenly flex flex-row md:flex-col dark:shadow-gray-900">
+                <GoHomeFill size="3rem" className="hover:scale-125 cursor-pointer" style={style}/>
+                <FaLaptopCode size="3rem" className="hover:scale-125 cursor-pointer" style={style}/>
+                <IoInformationCircle size="3rem" className="hover:scale-125 cursor-pointer" style={style}/>
+                {theme === "dark" ? <MdLightMode className="hover:scale-125 cursor-pointer" style={style} size="3rem" onClick={()=>{setTheme("light")}} /> : <MdDarkMode className="hover:scale-110 cursor-pointer" style={style} size="3rem" onClick={()=>{setTheme("dark")}}/>}
                 </div>
         </div>
         </>
