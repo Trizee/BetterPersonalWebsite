39851b4b3f277439af47a4adc3b995a642922b27
Tri <trihoang09@gmail.com>
2024-01-16 16:43:15 -0700

    working on home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,7 +8,7 @@ export default function Home(){
             animate={{y: 0, opacity:1}}
             exit={{x: window.innerWidth, opacity:1, transition:{duration:0.2}}}
             >
-            <div className="flex w-100 md:w-[30rem] items-center">
+            <div className="flex w-full md:w-[30rem] items-center">
                 <img src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png" className="h-14 w-14" alt="profile picture" />
                 <div className="px-6">
                     <p className="text-lg">Tri Hoang</p>
