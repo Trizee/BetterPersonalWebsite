80b5fecbd9caf96ee0374e5892467cb33cb9707b
Tri <trihoang09@gmail.com>
2024-01-16 14:25:07 -0700

    route animation

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -4,9 +4,9 @@ export default function Blog(){
     return(
         <motion.div 
             className=""
-            initial={{translateX: -50, opacity:0}}
-            animate={{translateX: 0, opacity:1}}
-            exit={{translateX: 50, opacity:0}}
+            initial={{width: "90%", opacity:0}}
+            animate={{width: "100%", opacity:1}}
+            exit={{x: window.innerWidth, opacity:1, transition:{duration:0.2}}}
             >
                 <p className="text-center ">Blog</p>
         </motion.div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,9 +4,9 @@ export default function Home(){
     return(
         <motion.div 
             className=""
-            initial={{translateX: -50, opacity:0}}
-            animate={{translateX: 0, opacity:1}}
-            exit={{translateX: 50, opacity:0}}
+            initial={{width: "90%", opacity:0}}
+            animate={{width: "100%", opacity:1}}
+            exit={{x: window.innerWidth, opacity:1, transition:{duration:0.2}}}
             >
                 <p className="text-center ">Home</p>
         </motion.div>
